52da62cd37714cd83921a139e85be50c9fd382a7
Arthur Streeter <astreeter@byui.edu>
2018-10-27 15:55:43 -0500

    Lesson 6

---
+++ assignments/lesson-6/scripts/javascript.js
@@ -0,0 +1,21 @@
+function showMenu() {
+    var navMenu = document.querySelectorAll('li:not(.menu)');
+
+    for (var i = 0; i < navMenu.length; i++) {
+        if (navMenu[i].style.display == 'none'){
+        navMenu[i].style.display = 'block';}
+        else{
+            navMenu[i].style.display = 'none'
+        }
+    }
+}
+
+// hides menu in case client clicks off of it
+window.onclick = function(event){
+    if (!event.target.matches('button.navMenu')){
+        var navMenu = document.querySelectorAll('li:not(.menu)');
+        for (var i = 0; i < navMenu.length; i++) {
+                navMenu[i].style.display = 'none'
+        }
+    }
+}
