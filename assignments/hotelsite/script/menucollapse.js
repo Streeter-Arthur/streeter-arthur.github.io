77f42bd72e41c531a3c78be6778ded6dafbec0ef
Arthur Streeter <astreeter@byui.edu>
2018-12-16 03:45:10 -0600

    Uploading for json

---
+++ assignments/hotelsite/script/menucollapse.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$("button.navMenu").click(function() {
+		$("nav ul li").not(document.getElementById("menu")).slideToggle("1000");
+	});
+     });
+    $(window).resize(function(){
+	    if ($(window).width() >= 762){	
+		    $("nav ul li").not(document.getElementById("menu")).show();
+	    }	
+	    if ($(window).width() <= 761){	
+		    $("nav ul li").not(document.getElementById("menu")).hide();
+	    }	
+    });
+$(document).ready(function() {
+	$("button.showSchedule").click(function() {
+		$("ul.schedule").slideToggle("1000");
+	});
+     });
---
+++ assignments/hotelsite/script/templeclosures.js
@@ -0,0 +1,4 @@
+var section = document.querySelector('ul.schedule');
+var requestURL = 'https://byui-cit230.github.io/weather/data/templeclosures.json';
+var request = new XMLHttpRequest();
+
